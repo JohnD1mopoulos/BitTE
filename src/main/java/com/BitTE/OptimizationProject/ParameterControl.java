c8f2dc1642fca180c174535a971c16a80f9e7c09
Romain <t8230028@aueb.gr>
2024-11-23 11:35:05 +0200

    Creation of ParameterControl file

---
+++ src/main/java/com/BitTE/OptimizationProject/ParameterControl.java
@@ -0,0 +1,33 @@
+/**
+ * Created to contain all methods used for constraint control
+ 
+ * This class will be effective in arranging the "workspace" ensuring efficient workflow.
+ */
+
+ import java.util.ArrayList;
+
+ class ParameterControl {
+     /**
+        * Used to show the remaining weight that can be added in the knapsack
+        *
+        * @param s Uses the ArrayList essentialItems
+        * @return Float variable that represents difference between the maximum weight value and the current weight value
+        */
+        protected static boolean checkWeight(){
+           //corresponding code
+        }
+
+        protected static boolean checkVolume(){
+            //corresponding code
+
+        }
+
+        protected static float getRemainingWeight(){
+           //corresponding code  
+        }
+
+        protected static float getRemainigVolume(){
+           //corresponding code
+        }
+    
+}
