09270404528dccf45329bb77c01eb9b3fca8500b
Romain <t8230028@aueb.gr>
2024-11-23 10:19:29 +0200

    Creation of Knapsack file

---
+++ src/main/java/com/BitTE/OptimizationProject/Knapsack.java
@@ -0,0 +1,10 @@
+/**
+ * Represents a dynamic knapsack used to store items, both essential and non-essential.
+ 
+ * The knapsack is designed to operate under specific weight and volume constraints. 
+ * If these constraints are not met during the addition of items, the operation 
+ * may terminate or fail to add the item depending on if the item is or isn't essential.
+ 
+ * This class can dynamically adjust to accommodate different types of items, 
+ * ensuring efficient use of available space and weight capacity.
+ */
