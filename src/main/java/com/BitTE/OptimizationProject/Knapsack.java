c4c34afc4ff6f0d6dfdd426a3e73914c36ac3aa6
Romain <t8230028@aueb.gr>
2024-11-24 15:40:55 +0200

    Add helper methods: checkWeight, checkVolume, checkConstraints and deleteItem.                                             Update method fillEssential to integrate new helper methods

--- src/main/java/com/BitTE/OptimizationProject/Knapsack.java
+++ src/main/java/com/BitTE/OptimizationProject/Knapsack.java
@@ -11,11 +11,12 @@
 
  //
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
  class Knapsack{
     // Static ArrayLists shared across all methods in this class
-    private static final ArrayList<Object> essentialItems = new ArrayList<>();//Using Object as data type until we have 
+    protected static final ArrayList<Object> essentialItems = new ArrayList<>();//Using Object as data type until we have 
     private static final ArrayList<Object> nonEssentialItems = new ArrayList<>();//the corresponding classes
     //Scanner object for user to input choices
     private static Scanner scanner = new Scanner(System.in);
@@ -29,46 +30,96 @@ import java.util.Scanner;
     */
 
     // TODO: Impliment the item classes when created and make the corresponding changes
-    private static boolean essential(float weight, float volume){
+    private static boolean fillEssential(float MaxWeight, float MaxVolume){
         
-        boolean volumeMet = true;
-        boolean weightMet = true;
-        boolean stopInput = false;
-        float totalWeight = 0;
-        float totalVolume = 0;
+        
+        
         System.out.println("Dear user, please choose the items that are essential for you: ");
+        boolean processRunning = true;
 
-        while (volumeMet != false && weightMet != false && stopInput != true){
-            System.out.println("Press 1 for X1/n"
-                              +"Press 2 for X2/n"
-                              +"Press 3 for X3/n"
-                              +"Press 4 for X4/n"
-                              +"Press 5 for X5/n");
+        while (processRunning){
+            //Display MENU 
+            System.out.println("Press 1 to add X1/n"
+                              +"Press 2 to add  X2/n"
+                              +"Press 3 to add X3/n"
+                              +"Press 4 to add X4/n"
+                              +"Press 5 to add X5/n");
 
-            int choice = scanner.nextInt();
+            //Input item
+            ParameterControl.inputItem();
 
-            switch (choice){
-                /*case 1:
-                    essentialItems.add(Object X1);
-                    System.out.println("Item "+ "Object "+"was added succesfully");//Object will be changed with the class item when 
-                    //are created
-                    break;
-                case 2:
-                    essentialItems.add(Object X2);
-                    System.out.println("Item "+ "Object "+"was added succesfully");
-                case 3:
-                    essentialItems.add(Object X3);
-                    System.out.println("Item "+ "Object "+"was added succesfully");  
-                case 4:
-                    essentialItems.add(Object X4);
-                    System.out.println("Item "+ "Object "+"was added succesfully");
-                case 5:
-                    essentialItems.add(Object X5);
-                   System.out.println("Item "+ "Object "+"was added succesfully");         
-            };*/
+            //Check constraints
+            int constraintsMet = ParameterControl.checkConstraints(MaxWeight, MaxVolume);
 
+            //Provide feedback based on constraints
+            switch (constraintsMet){
+                case 1: //Constraints respected
+                        System.out.println("You still have "+ParameterControl.getRemainingWeight(MaxWeight)+" available kgs"
+                        +"and "+ParameterControl.getRemainigVolume(MaxVolume)+" available cm2.");
+                        break;
+                case 2: //Volume constraint not respected
+                        System.out.println("You still have "+ParameterControl.getRemainingWeight(MaxWeight)+" available kgs"
+                        +"and have surpassed the maximum volume by"+(-ParameterControl.getRemainigVolume(MaxVolume))+" by cm2.");
+                        break;
+                case 3: //Weigth constraint not respected
+                        System.out.println("You have surpassed the maximum weigth by "+(-ParameterControl.getRemainingWeight(MaxWeight))
+                        +"kgs. But you still have "+ParameterControl.getRemainigVolume(MaxVolume)+" available cm2.");   
+                        break;
+                case 4: //Both constraints not respected
+                        System.out.println("You have surpassed the maximum weigth by "+(-ParameterControl.getRemainingWeight(MaxWeight))
+                        +"kgs. You also have surpassed the maximum volume by"+(-ParameterControl.getRemainigVolume(MaxVolume))
+                        +" by cm2.");
+                        break;   
+            }
 
-
-        }   
+            //Handle different constraint scenarios
+            if (constraintsMet != 1){//Constraints arent met
+                System.out.println("Press 1 to terminate process.\n"
+                                  +"Press 2 to remove item");
+                boolean validChoice;
+                while (!validChoice){
+                    try {
+                        int userChoice = scanner.nextInt();
+                        scanner.nextLine();
+                        if (userChoice == 1){
+                            return false;//Knapsack wasnt filled 
+                        } else if (userChoice == 2){
+                            ParameterControl.deleteItem();
+                            validChoice = true;//stop loop
+                        } else {
+                            System.out.println("Invalid choice. Please enter 1 or 2");
+                        }
+                    } catch (InputMismatchException e){
+                         System.out.println("Invalid input. Please enter a valid integer.");
+                         scanner.nextLine();
+                    }
+                }
+            } else {//Constraints are met
+                
+                System.out.println("Press 1 to terminate process.\n"
+                                  +"Press 2 to add another item");
+                
+                 boolean validChoice;
+                 while (!validChoice){
+                     try {
+                         int userChoice = scanner.nextInt();
+                         scanner.nextLine();
+                         if (userChoice == 1){
+                            System.out.println("All esential items have been added!!!");
+                             return true;//Knapsack was filled 
+                        } else if (userChoice == 2) {
+                             validChoice = true;//Continue adding items
+                        } else {
+                            System.err.println("INvalid choice. Please press 1 oe 2.");
+                        }
+                     } catch (InputMismatchException e){
+                             System.out.println("Invalid input. Please enter a valid integer.");
+                             scanner.nextLine();
+                     }
+                 }
+            
+            }
+        } 
+        return false; 
     }
  }
--- src/main/java/com/BitTE/OptimizationProject/ParameterControl.java
+++ src/main/java/com/BitTE/OptimizationProject/ParameterControl.java
@@ -4,7 +4,9 @@
  * This class will be effective in arranging the "workspace" ensuring efficient workflow.
  */
 
- import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
  class ParameterControl {
      /**
@@ -13,21 +15,156 @@
         * @param s Uses the ArrayList essentialItems
         * @return Float variable that represents difference between the maximum weight value and the current weight value
         */
-        protected static boolean checkWeight(){
-           //corresponding code
+        protected static float checkWeight(){
+           float totalWeight= 0;
+           for (int i =0; i < Knapsack.essentialItems.size(); i++){
+            totalWeight += Knapsack.essentialItems.get(i).getWeight();
+           }
+           return totalWeight;
         }
 
-        protected static boolean checkVolume(){
-            //corresponding code
-
+        protected static float checkVolume(){
+            float totalVolume= 0;
+            for (int i =0; i < Knapsack.essentialItems.size(); i++){
+             totalWeight += Knapsack.essentialItems.get(i).getVolume();
+            }
+            return totalVolume;
         }
 
-        protected static float getRemainingWeight(){
-           //corresponding code  
+        protected static float getRemainingWeight(float MaxWeight){
+           return  MaxWeight - checkWeight();
         }
 
-        protected static float getRemainigVolume(){
-           //corresponding code
+        protected static float getRemainigVolume(float MaxVolume){
+           return MaxVolume - checkVolume();
         }
+
+        protected static int checkConstraints(float MaxWeight, float MaxVolume){
+
+            boolean weightConstraintRespected = getRemainingWeight(MaxWeight) > 0;;
+            boolean volumeConstraintRepsected = getRemainingVolume(MaxVolume) > 0;;
+            
+    
+            if (weightConstraintRespected && volumeConstraintRepsected){
+                return 1;
+            }else if (weightConstraintRespected){
+
+                return 2;
+            }else if (volumeConstraintRepsected){
+                return 3;
+            }else {
+                return 4;
+            }
+        }
+
+
+        protected static void showItems(){
+            for (int i =0; i < Knapsack.essentialItems.size(); i++){
+                System.out.println((i+1) + ") " + Knapsack.essentialItems.get(i));
+            }
+        }
+
+        protected static void inputItem(){
+            Scanner scanner = new Scanner(System.in);
+            int choiceOfItem = -1;
+            
+            try {
+                choiceOfItem = scanner.nextInt();
+                switch (choiceOfItem){
+                    /*case 1:
+                        essentialItems.add(Object X1);
+                        System.out.println("Item "+ "Object "+"was added succesfully");//Object will be changed with the class item when 
+                        //are created
+                        break;
+                    case 2:
+                        essentialItems.add(Object X2);
+                        System.out.println("Item "+ "Object "+"was added succesfully");
+                    case 3:
+                        essentialItems.add(Object X3);
+                        System.out.println("Item "+ "Object "+"was added succesfully");  
+                    case 4:
+                        essentialItems.add(Object X4);
+                        System.out.println("Item "+ "Object "+"was added succesfully");
+                    case 5:
+                        essentialItems.add(Object X5);
+                       System.out.println("Item "+ "Object "+"was added succesfully"); */        
+                };
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please select a valid integer");
+            }
+        }
+
+        
+        /**
+        * Deletes a number of items from ArrayList essentialItems depending on the user's input
+        * @param s None
+        * @return Nothing
+        */
+        // TODO: Waiting the classes to be created so that i can be adjusted for ArrayList nonessenitalItems also
+        protected static void deleteItem() {
+            Scanner scanner = new Scanner(System.in);
+        
+            while (true) {
+                // Check if there are items to delete
+                if (Knapsack.essentialItems.isEmpty()) {
+                    System.out.println("No items available to delete.");
+                    return; // Exit the method
+                }
+        
+                // Display available items
+                System.out.println("You can delete the following items:");
+                showItems();
+        
+                // Prompt the user to choose an item to delete
+                System.out.println("Which one do you want to delete? (Press 0 to cancel)");
+        
+                int choice1 = -1; // Variable representing the item to be deleted
+                try {
+                    choice1 = scanner.nextInt();
+                    scanner.nextLine(); 
+        
+                    if (choice1 == 0) {
+                        System.out.println("Stopping deletion of items.");
+                        return; // Exit the method
+                    } else if (choice1 < 1 || choice1 > Knapsack.essentialItems.size()) {
+                        System.out.println("Invalid choice. Please select a valid item number.");
+                        continue; // Restart the loop
+                    }
+        
+                    // Delete the chosen item
+                    System.out.println("Item no " + choice1 + " (" + Knapsack.essentialItems.get(choice1 - 1) + ") has been deleted.");
+                    Knapsack.essentialItems.remove(choice1 - 1);
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    scanner.nextLine(); 
+                    continue; // Restart the loop
+                }
+        
+                // Ask the user if they want to delete another item
+                System.out.println("Press 1 to delete another item.\nPress 2 to stop deleting items.");
+                int choice2 = -1; // Variable representing choice to continue or stop method
+        
+                while (true) {
+                    try {
+                        choice2 = scanner.nextInt();
+                        scanner.nextLine(); 
+        
+                        if (choice2 == 1) {// Continue the loop to delete another item
+                            break;
+                        } else if (choice2 == 2) {
+                            System.out.println("Stopping deletion process.");
+                            return; // Exit the method
+                        } else {
+                            System.out.println("Invalid choice. Please press 1 to delete another item or 2 to stop.");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Please enter 1 or 2.");
+                        scanner.nextLine();
+                    }
+                }
+            }
+        }
+        
     
 }
