6bb7c97a6f89a8527e47444035a1d1235e99f3f1
ALIKI POTAMIANAKI <t8230124@aueb.gr>
2024-12-19 19:56:35 +0200

    Add Nonessentials and CreateSuitcase classes

---
+++ src/main/java/com/BitTE/OptimizationProject/CreateSuitcase.java
@@ -0,0 +1,25 @@
+package main.java.com.BitTE.OptimizationProject;
+
+import java.util.Scanner;
+
+public class CreateSuitcase {
+    //Static variables represent the allowable weigth in kilos and the volume in cubic meters of the suitcase
+    protected static double maxVolume;
+    protected static double maxWeight;
+
+    //Method which asks the customer about the dimensions of the suitcase and calculates the volume
+    private static void suitcaseCharacteristics() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the desired weight of the suitcase (in kilograms): ");
+            maxWeight = scanner.nextDouble();
+        System.out.print("Enter the length of the suitcase (in meters): ");
+            double length = scanner.nextDouble();
+        System.out.print("Enter the width of the suitcase (in meters): ");
+            double width = scanner.nextDouble();
+        System.out.print("Enter the height of the suitcase (in meters): ");
+            double height = scanner.nextDouble();
+        //Calculating volume
+        maxVolume = length * width * height;
+        scanner.close();
+    }
+}
