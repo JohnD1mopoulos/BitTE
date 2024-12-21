29c6735b7dae8b1e29c35e5cb2c61cfeb4671b6c
Georgia Seinti <t8230132@aueb.gr>
2024-12-21 09:27:29 +0200

    Modify AppStart file to ensure connectability between the UI pages

--- src/main/java/com/BitTE/OptimizationProject/AppStart.java
+++ src/main/java/com/BitTE/OptimizationProject/AppStart.java
@@ -1,10 +1,10 @@
 package com.BitTE.OptimizationProject;   //Added the necessary package
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class AppStart extends Application {
 
@@ -17,6 +17,16 @@ public class AppStart extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+	
+	 public void switchToSecondPage(Stage primaryStage) throws IOException {
+        
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SecondPage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("PackApp");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
 
     public static void main(String[] args) {
         launch(args);
