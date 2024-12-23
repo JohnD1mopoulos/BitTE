5bf1941a4316587ae23075e210f567aae7a99dd7
markoulinos <balaskasmarkos@gmail.com>
2024-12-23 12:25:37 +0000

    Add properties to Clothing to ensure safety with connection to database

--- src/main/java/com/BitTE/OptimizationProject/Clothing.java
+++ src/main/java/com/BitTE/OptimizationProject/Clothing.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Clothing extends PackingItem {
     private static final Logger logger = LoggerFactory.getLogger(Clothing.class);
+    private Properties properties = new Properties();
     private final String gender;
 
     /**
