c5bb0686aa542a56dec335c67ecec8a5137eb8ea
markoulinos <balaskasmarkos@gmail.com>
2025-01-17 11:58:16 +0000

    Add tests for constructor in PackingItemTest

--- src/test/java/com/BitTE/OptimizationProject/PackingItemTest.java
+++ src/test/java/com/BitTE/OptimizationProject/PackingItemTest.java
@@ -8,9 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PackingItemTest {
     @Test
+    void testConstructorForEssentialItems() {
+        PackingItem packingItem = new Clothing("T-Shirt", 'M', 'M');
+        assertEquals(0, packingItem.getValue(), "The value should be 0.");
+        assertEquals("T-Shirt", packingItem.getType(), "The type should be T-Shirt.");
+        assertEquals('M', packingItem.getSize(), "The size should be M.");
+        assertEquals('M', packingItem.getGender(), "The gender should be M");
+    }
+
+    @Test
+    void testConstructorForNonEssentialItems() {
+        PackingItem packingItem = new Clothing(1, "T-Shirt", 'M', 'M');
+        assertEquals(1, packingItem.getValue(), "The value should be 1.");
+        assertEquals("T-Shirt", packingItem.getType(), "The type should be T-Shirt.");
+        assertEquals('M', packingItem.getSize(), "The size should be M.");
+        assertEquals('M', packingItem.getGender(), "The gender should be M");
+    }
+
+    @Test
     void testValidateAttributeWithValidAttribute() {
         PackingItem packingItem = new Clothing("T-Shirt", 'M', 'M');
-
         assertDoesNotThrow(() -> {
             packingItem.validateAttribute("volume");
             packingItem.validateAttribute("weight");
@@ -20,14 +37,10 @@ public class PackingItemTest {
     @Test
     public void testValidateAttributeWithInvalidAttribute() {
         PackingItem packingItem = new Clothing("T-Shirt", 'M', 'M');
-
-        // Expect IllegalArgumentException for an invalid attribute
         String invalidAttribute = "height";
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             packingItem.validateAttribute(invalidAttribute);
         });
-
-        // Verify the exception message
         assertEquals("Invalid attribute: " + invalidAttribute, exception.getMessage());
     }
 }
