ffbd70e79bef988f22e9a878dbb0aec953095c75
markoulinos <balaskasmarkos@gmail.com>
2025-01-18 20:31:28 +0000

    Create package-info file to fit checkstyle requirements

---
+++ src/main/java/com/BitTE/OptimizationProject/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * Provides the classes necessary for the BitTE Optimization Project, encompassing database management,
+ * user interactions, and data handling for optimized packing.
+ *
+ * Classes include:
+ * - {@link com.BitTE.OptimizationProject.AppStart} as the main entry point for the application.
+ * - {@link com.BitTE.OptimizationProject.Clothing} to handle clothing items.
+ * - {@link com.BitTE.OptimizationProject.CreateSuitcase} for creating suitcase packing scenarios.
+ * - {@link com.BitTE.OptimizationProject.DatabaseConnection} for managing database connections.
+ * - {@link com.BitTE.OptimizationProject.DatabaseTableCreation} for database schema creation and initial data setup.
+ * - {@link com.BitTE.OptimizationProject.EssentialConstraints} to define constraints for essential items.
+ * - {@link com.BitTE.OptimizationProject.EssentialItems} to manage essential items.
+ * - {@link com.BitTE.OptimizationProject.Extras} to handle extra items.
+ * - {@link com.BitTE.OptimizationProject.ItemDeletionHandler} to manage item deletions.
+ * - {@link com.BitTE.OptimizationProject.ItemInputHandler} to handle input of new items.
+ * - {@link com.BitTE.OptimizationProject.MenuHandler} for user interface menus.
+ * - {@link com.BitTE.OptimizationProject.NonEssentialItems} to manage non-essential items.
+ * - {@link com.BitTE.OptimizationProject.PackingItem} as the base class for Clothing and Extras items.
+ * - {@link com.BitTE.OptimizationProject.ResultPresenter} to present results of the packing process.
+ * - {@link com.BitTE.OptimizationProject.SpaceOptimizer} for optimizing space within the suitcase.
+ *
+ * This package aims to provide a comprehensive suite of tools for efficient packing and management of items in various scenarios.
+ */
+package com.BitTE.OptimizationProject;
